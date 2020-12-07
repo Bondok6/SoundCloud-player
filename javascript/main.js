d1000aa28fc687d0db9dd9a53db6822e4331dd75
kyrillos <kyrilloshany99@gmail.com>
2020-12-07 23:18:11 +0200

    modify

--- javascript/main.js
+++ javascript/main.js
@@ -3,17 +3,29 @@
 
 
 // soundCloud Api
+var soundCloudApi = {};
+soundCloudApi.init = function () {
+  
+  SC.initialize({
+    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+  });
 
-SC.initialize({
-  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
+}
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'rilo kiley'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+soundCloudApi.init();
+
+soundCloudApi.getTrack = function (inputValue) {
+  
+  SC.get('/tracks', {
+    q: inputValue
+  }).then(function(tracks) {
+    console.log(tracks);
+  });
+
+}
+
+soundCloudApi.getTrack('rilo kiley');
+
 
 // display the cards 
 
